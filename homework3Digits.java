d89df786e046fe0062b31688733836b0ad6dc01d
javaDmitry74 <76561015+javaDmitry74@users.noreply.github.com>
2021-01-13 14:52:31 +0300

    Latest update

--- homework3Digits.java
+++ homework3Digits.java
@@ -5,31 +5,37 @@ public class homework3Digits {
         int c = 2395;
         int average = (a + b + c) / 3;
         if (a > b && a > c && c < b && c < a) {
+            System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
             System.out.println("A is highest number and it " + a);
             System.out.println(" and C is smallest number " + c);
-            System.out.println("Average of all our numbers is = " + average);
+                        System.out.println("Average of all our numbers is = " + average);
         } else {
             if (a > b && a > c && b < c && b < a) {
+                System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
                 System.out.println("A is highest number and it " + a);
                 System.out.println(" and b is smallest number " + b);
                 System.out.println("Average of all our numbers is = " + average);
             } else {
                 if (b > a && b > c && c < b && c < a) {
+                    System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
                     System.out.println("B is highest number and it " + b);
                     System.out.println(" and C is smallest number " + c);
                     System.out.println("Average of all our numbers is = " + average);
                 } else {
                     if (b > c && b > a && a < b & a < c) {
+                        System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
                         System.out.println("B is highest number and it " + b);
                         System.out.println(" and A is smallest number " + a);
                         System.out.println("Average of all our numbers is = " + average);
                     } else {
                        if (c > a && c > b && a < b && a < c) {
-                        System.out.println("C is highest number and it " + c);
-                        System.out.println(" and A is smallest number " + a);
-                        System.out.println("Average of all our numbers is = " + average);
+                           System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
+                           System.out.println("C is highest number and it " + c);
+                           System.out.println(" and A is smallest number " + a);
+                           System.out.println("Average of all our numbers is = " + average);
                     } else  {
                            if (c > a && c > b && b < a && b < c) {
+                               System.out.println("We have 3 numbers : " + a +", " + b + ", " + c );
                                 System.out.println("C is highest number and it " + c);
                                 System.out.println(" and B is smallest number " + b);
                                 System.out.println("Average of all our numbers is = " + average);
